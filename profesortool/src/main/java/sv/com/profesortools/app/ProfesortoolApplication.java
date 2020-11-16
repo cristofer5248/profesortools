3f278c28bd9cbdb40bef6406a06ae59292c27a56
chris <chris@DESKTOP-4290HK4>
2020-11-15 19:30:18 -0600

    mi app nueva para profesores

---
+++ profesortool/src/main/java/sv/com/profesortools/app/ProfesortoolApplication.java
@@ -0,0 +1,13 @@
+package sv.com.profesortools.app;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ProfesortoolApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ProfesortoolApplication.class, args);
+	}
+
+}
---
+++ profesortool/src/main/java/sv/com/profesortools/app/ServletInitializer.java
@@ -0,0 +1,13 @@
+package sv.com.profesortools.app;
+
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
+
+public class ServletInitializer extends SpringBootServletInitializer {
+
+	@Override
+	protected SpringApplicationBuilder configure(SpringApplicationBuilder application) {
+		return application.sources(ProfesortoolApplication.class);
+	}
+
+}
